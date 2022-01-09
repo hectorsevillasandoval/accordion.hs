41e56e8198a762405416891c025d8a8e21ab282e
hsevilla <sevillasandoval.hector@gmail.com>
2022-01-09 01:03:31 -0600

    Adding look and feel to the example, finishing library

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -6,19 +6,38 @@
 function AccordionJS(container = null) {
 	if (!container) throw new Error('Please specify a container');
 
-    this.itemClass = 'accordion__item';
+	this.itemClass = 'accordion__item';
+	this.activeItemClass = 'accordion__item--open';
 
-    this.init();
-	
+	this.init();
 }
 
-AccordionJS.prototype.init = function(){
-    // add Event Lister to the document
-    document.body.addEventListener('click', e => {
-        if(e.target.parentNode.className.includes(this.itemClass)){
-            const currentItem = e.target.parentNode.parentNode;
-            currentItem.classList.add('accordion__item--open');
-        }
-        
-    });
-}
+AccordionJS.prototype.init = function () {
+	// add Event Lister to the document
+	document.body.addEventListener('click', (e) => {
+		if (e.target.className.includes(this.itemClass)) {
+			this.currentItem = e.target.parentNode;
+			if (this.currentItem.className.includes(this.activeItemClass)) {
+				this.toggle();
+				return;
+			}
+            this.close();
+            this.open();
+		}
+	});
+};
+
+AccordionJS.prototype.open = function () {
+    console.log('open');
+	this.currentItem.classList.add('accordion__item--open');
+};
+AccordionJS.prototype.toggle = function () {
+	console.log('toggle');
+    this.currentItem.classList.toggle('accordion__item--open');
+};
+AccordionJS.prototype.close = function () {
+    console.log('close');
+    const currentAccordion = this.currentItem.closest('.accordion');
+    const openedItem = currentAccordion.querySelector('.accordion__item--open');
+    if( openedItem ) openedItem.classList.remove('accordion__item--open');
+};
